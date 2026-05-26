3f1fc6292e068b64bacca588af4e38928e5e0291
Lyhuor168 <lyhourheoun04@gmail.com>
2026-05-26 10:43:19 +0700

    fix: contactus/aboutus/services use public layout (layouts.app)

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('title', 'ទំនាក់ទំនង - Contact Us')
 
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('title', 'សេវាកម្ម - Services')
 
